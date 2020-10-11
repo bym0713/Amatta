f16f30ceb884a773dd13148a215221525f9ef074
bym0713 <72542402+bym0713@users.noreply.github.com>
2020-10-11 11:14:50 +0900

    Create firebase_init.js

---
+++ firebase_init.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDs1VvFjMbGXq8rovOJA3rk1ahEVHp5x_4",
+    authDomain: "double-skyline-265803.firebaseapp.com",
+    databaseURL: "https://double-skyline-265803.firebaseio.com",
+    projectId: "double-skyline-265803",
+    storageBucket: "double-skyline-265803.appspot.com",
+    messagingSenderId: "142102862405",
+    appId: "1:142102862405:web:93b93997b4c6d2ad51a12c",
+    measurementId: "G-FRS6TDK1N2"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    
